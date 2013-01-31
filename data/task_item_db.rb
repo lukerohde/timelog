5e3e217214a35d9851a1c0044df3f8b7756e509b
Luke Rohde <lukerohde@gmail.com>
2013-01-31 19:06:51 +1100

    task ws working no time ws

--- data/task_item_db.rb
+++ data/task_item_db.rb
@@ -1,22 +1,19 @@
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/timelog.sqlite3")
 
-class TimeItem
-  include DataMapper::Resource #DB time items
-  storage_names[:legacy] = 'time_items'
-  property :id, Serial, :required => true, :key => true
-  property :task, Text, :required => true
-  property :time_secs, Integer, :required => true
-  property :start_time, DateTime, :required => true
+class TaskItem
+  include DataMapper::Resource #DB task items
+
+  property :name, Text, :required => true, :key => true
+  property :notes, Text
   
+  #WS time item method
   def to_json(*a)
    {
-     'id' => self.id,
-     'task' => self.task,
-     'time_secs' => self.time_secs,
-     'start_time' => self.start_time,
+     'name' => self.name,
+     'notes' => self.notes,
    }.to_json(*a)
   end
 end
 
 DataMapper.finalize
-TimeItem.auto_upgrade!
+TaskItem.auto_upgrade!
